0f25cfc0689dec733ca5d809a22c9e92a53418ae
carlos.garita <carlos.garita@factork-software.com>
2024-09-21 22:34:22 -0600

    Configuration

--- firebase.js
+++ firebase.js
@@ -7,13 +7,20 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDdjkEgoITD4UWYqQ6MXG_5zNvBo_1bPnY",
-  authDomain: "adihi-test.firebaseapp.com",
-  projectId: "adihi-test",
-  storageBucket: "adihi-test.appspot.com",
-  messagingSenderId: "685939645391",
-  appId: "1:685939645391:web:ac816246fcfeb86a0510ba",
-  measurementId: "G-VTGWKP7GD5"
+//   apiKey: "AIzaSyDdjkEgoITD4UWYqQ6MXG_5zNvBo_1bPnY",
+//   authDomain: "adihi-test.firebaseapp.com",
+//   projectId: "adihi-test",
+//   storageBucket: "adihi-test.appspot.com",
+//   messagingSenderId: "685939645391",
+//   appId: "1:685939645391:web:ac816246fcfeb86a0510ba",
+//   measurementId: "G-VTGWKP7GD5"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
