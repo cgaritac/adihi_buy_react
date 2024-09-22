7e8a04bb0a243b6a21aac47a14c5fec93e7bac8a
carlos.garita <carlos.garita@factork-software.com>
2024-09-21 23:14:13 -0600

    App.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
-function App() {
+import { MainLayout } from '../src/components/layout'
+import { Home } from '../src/pages/home'
+import { Category } from '../src/pages/category'
+import { Katanas } from '../src/pages/katanas'
+import { New } from '../src/pages/new'
+import { Popular } from '../src/pages/popular'
+import { Calendar } from './pages/calendar'
+import { Contact } from '../src/pages/contact'
+import { Terms } from '../src/pages/terms'
+import { Cart } from '../src/pages/cart'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+function App() {
   return (
     <>
-      <h1>Hola Mundo cruel mas</h1>
+      <>     
+        <BrowserRouter> 
+          <MainLayout>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/category' element={<Category/>}/>
+              <Route path='/katanas' element={<Katanas/>}/>
+              <Route path='/new' element={<New/>}/>
+              <Route path='/popular' element={<Popular/>}/>
+              <Route path='/calendar' element={<Calendar/>}/>
+              <Route path='/contact' element={<Contact/>}/>
+              <Route path='/terms' element={<Terms/>}/>
+              <Route path='/cart' element={<Cart/>}/>
+            </Routes>
+          </MainLayout>
+        </BrowserRouter>
+    </>
     </>
   )
 }
