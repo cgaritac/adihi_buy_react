fd9bd2bae2c89d15f576a2afb62632049bc96e82
carlos.garita <carlos.garita@factork-software.com>
2024-09-22 22:54:34 -0600

    Login config

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,14 +2,25 @@ import { useState } from 'react';
 import 'firebase/auth';
 import { ButtonComponent } from '../components/common/button';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
 
 export const LoginUser = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const firebaseConfig = {
+        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_FIREBASE_APP_ID,
+      };
+      const app = initializeApp(firebaseConfig);
+      const auth = getAuth(app);
  
 
     const submit = () => {
-        const auth = getAuth();
+
         signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
 
